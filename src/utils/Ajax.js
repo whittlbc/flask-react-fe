7f674d4cbe8ddb00c191712b33bd057dd7f02e4e
Ben Whittle <benwhittle31@gmail.com>
2017-08-26 09:58:11 -0700

    comments

---
+++ src/utils/Ajax.js
@@ -0,0 +1,62 @@
+/*
+  Wrapper around whatwg-fetch for making ajax requests
+
+  Usage example (no usage difference between get/post/put/delete):
+
+    Ajax.get('/api/users', { company: 'My Company' })
+      .then((resp) => resp.json()) // unpack json response data
+      .then((data) => {
+        console.log(data);
+      });
+ */
+
+import $ from 'jquery';
+
+var Ajax;
+
+class AjaxClass {
+
+  get(url, params) {
+    return this.urlEncoded(url, params, 'GET');
+  }
+
+  post(url, params) {
+    return this.jsonRequst(url, params, 'POST');
+  }
+
+  put(url, params) {
+    return this.jsonRequst(url, params, 'PUT');
+  }
+
+  delete(url, params) {
+    return this.urlEncoded(url, params, 'DELETE');
+  }
+
+  urlEncoded(url, params, method) {
+    if (params) {
+      url += ('?' + $.param(params));
+    }
+
+    return fetch(url, { method: method });
+  }
+
+  jsonRequst(url, params, method) {
+    return fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params || {})
+    });
+  }
+}
+
+function getInstance() {
+  if (!Ajax) {
+    Ajax = new AjaxClass();
+  }
+
+  return Ajax;
+}
+
+export default getInstance();
